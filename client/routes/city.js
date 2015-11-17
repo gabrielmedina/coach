1b1e38d1a6c74c994bcad26f4055f1aa26008a64
gabrielmedina <gmedina.santos@gmail.com>
2015-11-17 09:27:26 -0300

    Adding routes for city

--- client/routes/city.js
+++ client/routes/city.js
@@ -3,6 +3,10 @@
 Router.route('city', {
   path: '/city',
 
+  waitOn: function(){
+    return Meteor.subscribe('cities');
+  },
+
   onBeforeAction: function(){
     if(Meteor.user().profile.type.value != 1){
       Router.go('/');
@@ -25,7 +29,17 @@ Router.route('cityCreate', {
 });
 
 Router.route('cityEdit', {
-  path: '/city/edit',
+  path: '/city/edit/:_id',
+
+  waitOn: function(){
+    return Meteor.subscribe('cities');
+  },
+
+  data: function(){
+    return {
+      _id: this.params._id
+    };
+  },
 
   onBeforeAction: function(){
     if(Meteor.user().profile.type.value != 1){
