9189a72667c85b9296922d5c66ebf9a815a087a1
gabrielmedina <gmedina.santos@gmail.com>
2016-02-03 20:02:57 -0300

    Changing operation of the app for best experience

--- client/views/training/index.js
+++ client/views/training/index.js
@@ -1,16 +1,16 @@
 // training index
 
 Template.training.helpers({
-  practitionerId: function(){
-    return this._id;
-  },
-
   trainings: function(){
     if(Meteor.user().profile.type.value === 3) {
       return Training.find({ $and: [{ practitioner: this._id }, { status: true }] } , {sort: {modifiedAt: -1}});
     } else {
       return Training.find({ practitioner: this._id }, {sort: {modifiedAt: -1}});
     }
+  },
+
+  practitionerId: function(){
+    return this._id;
   }
 });
 
