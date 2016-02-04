60853bca9669f10ca5bda7186b3b5e84ad63086a
gabrielmedina <gmedina.santos@gmail.com>
2016-02-04 10:22:13 -0300

    Adding package search-source

--- client/views/training/search.js
+++ client/views/training/search.js
@@ -1,11 +1,33 @@
 // training search
 
+var options = {
+  keepHistory: 1000 * 60 * 5,
+  localSearch: true
+};
+
+var fields = ['profile.name'];
+
+UsersSearch = new SearchSource('users', fields, options);
+
+Template.trainingSearch.rendered = function() {
+  UsersSearch.search('');
+};
+
 Template.trainingSearch.helpers({
   users: function(){
-    return Meteor.users.find({ 'profile.type.value': 3 });
+    return UsersSearch.getData({
+      sort: {isoScore: -1}
+    });
+  },
+
+  loading: function() {
+    return UsersSearch.getStatus().loading;
   }
 });
 
 Template.trainingSearch.events({
-
+  'keyup .input--search': _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    UsersSearch.search(text);
+  }, 200)
 });
